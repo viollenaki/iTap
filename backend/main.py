cfea5d02434ceedb8e9b0c38cb0c86e30491181e
Akbar Sharipov <tanakaakuustik@gmail.com>
2026-02-20 18:04:09 +0600

    added port

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 import uvicorn
+import os
+port = int(os.environ.get("PORT", 8000))  # Railway автоматически задаёт PORT
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
